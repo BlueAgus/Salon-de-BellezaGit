e10c2accb6b900997b2da8b0a194ff65549eac78
BlueAgus <micaelamassuco@gmail.com>
2024-10-31 16:54:18 -0300

    cambio agus
    cambio de prueba

--- Salon de Belleza/src/Main.java
+++ Salon de Belleza/src/Main.java
@@ -3,6 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("HOLA HICE UN CAMBIO ");
+        System.out.println("Holi soy agus jeje");
 
         }
     }
